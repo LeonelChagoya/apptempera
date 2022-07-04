49a57af99799e37b32c3bec963c91f7cf26d447d
LeonelChagoya <106455460+LeonelChagoya@users.noreply.github.com>
2022-07-04 18:48:29 -0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import Card from './components/card'
+import './App.css'
+
+function App() {
+
+  
+  return (
+
+    <div className='App'>
+      <Card />
+      
+    </div>
+      
+    
+  )
+}
+
+export default App
